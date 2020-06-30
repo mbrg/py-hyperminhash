3cd1d00fe1106095e253725198444725781c3f55
mibarg <mibarg@users.noreply.github.com>
2020-06-30 15:57:30 +0300

    failing test_cardinality

--- python/hyperminhash_test.py
+++ python/hyperminhash_test.py
@@ -1,6 +1,8 @@
+from typing import Dict
+
 import numpy as np
 
-from hyperminhash import Register, m, reg_sum_and_zeros
+from hyperminhash import Register, m, reg_sum_and_zeros, Sketch
 
 
 LETTER_BYTES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -36,3 +38,28 @@ def test_all_zeros():
 
 	_, got = reg_sum_and_zeros(registers)
 	assert got == exp, f"expected {exp:.2f}, got {got:.2f}"
+
+
+def rand_string_bytes_mask_impr_src(n: np.uint32) -> str:
+	return "".join(
+		[LETTER_BYTES[np.random.randint(0, len(LETTER_BYTES))] for 	_ in range(n)])
+
+
+def test_cardinality():
+	sk = Sketch()
+	step = 10000
+	unique: Dict[str, bool] = {}
+
+	for i in range(1, 1000000 + 1):
+		st = rand_string_bytes_mask_impr_src(np.uint32(np.random.randint(0, 32)))
+		b = str.encode(st)
+		sk.add(b)
+		unique[st] = True
+
+		if len(unique) % step == 0:
+			exact = np.uint64(len(unique))
+			res = np.uint64(sk.cardinality())
+			step *= 10
+
+			ratio = 100 * estimate_error(res, exact)
+			assert ratio <= 2, f"Exact {exact}, got {res} which is {ratio:.2f} error"
