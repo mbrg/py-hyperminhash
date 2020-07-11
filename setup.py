ad08f3aedccfe086f48e561aa4c43b8e0e1ed37f
mibarg <11074433+mibarg@users.noreply.github.com>
2020-07-11 15:41:47 +0300

    Remove dev reqs

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     license="MIT",
     author="mibarg",
     author_email="mibarg@users.noreply.github.com",
-    install_requires=["metrohash-python", "pytest-xdist", "numpy",  "cached-property"],
+    install_requires=["metrohash-python", "numpy",  "cached-property"],
     python_requires=">=3.6",
 )
